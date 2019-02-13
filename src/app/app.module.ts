4cb66a37423611179b78e7f8107332197a8cd637
Ashish S(Manjaro) <s.cricket.ashish@gmail.com>
2019-02-13 11:03:54 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroCardComponent } from './components/intro-card/intro-card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroCardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/intro-card/intro-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-card',
+  templateUrl: './intro-card.component.html',
+  styleUrls: ['./intro-card.component.css']
+})
+export class IntroCardComponent implements OnInit {
+  dismiss(e: any, t: any) {
+    let trigger: boolean =
+      e.target.className == 'container' || e.target.className == 'close';
+    if (trigger) {
+      t.style.display = 'none';
+    }
+  }
+  constructor() {}
+
+  ngOnInit() {}
+}
