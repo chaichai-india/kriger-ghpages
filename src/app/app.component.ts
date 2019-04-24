4c673719982c620a4eed0f704c07d6fe9b46fa23
Ashish S(Manjaro) <s.cricket.ashish@gmail.com>
2019-04-24 17:46:57 +0530

    introcard show every visit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,18 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'kriger-campus-website';
 
-  visitCount: number = +localStorage.getItem('visitCount');
-  showIntro: boolean = this.visitCount == 0 || this.visitCount % 20 == 0;
+  // visitCount: number = +localStorage.getItem('visitCount');
+  // showIntro: boolean = this.visitCount == 0 || this.visitCount % 20 == 0;
 
   constructor() {}
 
   ngOnInit() {
-    if (!this.visitCount) {
-      localStorage.setItem('visitCount', '1');
-    } else {
-      this.visitCount++;
-      localStorage.setItem('visitCount', this.visitCount.toString());
-    }
+    // if (!this.visitCount) {
+    //   localStorage.setItem('visitCount', '1');
+    // } else {
+    //   this.visitCount++;
+    //   localStorage.setItem('visitCount', this.visitCount.toString());
+    // }
     // console.log(this.visitCount, this.showIntro);
   }
 }
