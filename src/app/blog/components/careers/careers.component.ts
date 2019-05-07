bae08aa06e48bbd1cce7272351e9e7f827827668
Ashish S(Manjaro) <s.cricket.ashish@gmail.com>
2019-05-07 18:30:27 +0530

    audit fix career

--- src/app/blog/components/careers/careers.component.ts
+++ src/app/blog/components/careers/careers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-careers',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./careers.component.css']
 })
 export class CareersComponent implements OnInit {
+  indeedUrl: string =
+    'https://www.indeedjobs.com/kriger-campus-indias-education-network/_hl/en_US?cpref=JXWAtnzf3XW5aRnY2g_zouV7xMD8uRK8SCWKYC7QXG4';
+  constructor(public sanitizer: DomSanitizer) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
