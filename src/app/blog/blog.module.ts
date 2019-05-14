799f1b1b81b096677e9a0b4523edef22311f585d
Ashish S(Manjaro) <s.cricket.ashish@gmail.com>
2019-05-14 15:48:22 +0530

    newsletter added

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -6,20 +6,31 @@ import { BlogRoutingModule } from './blog-routing.module';
 import { InternshipsComponent } from './components/internships/internships.component';
 import { PerformersComponent } from './components/internships/performers/performers.component';
 import { InternsComponent } from './components/internships/interns/interns.component';
+import { MediacoverageComponent } from './components/mediacoverage/mediacoverage.component';
+import { CareersComponent } from './components/careers/careers.component';
+import { TncComponent } from './components/tnc/tnc.component';
+import { OurteamComponent } from './components/ourteam/ourteam.component';
+import { TeamMemberComponent } from './components/ourteam/team-member/team-member.component';
+import { NewsletterFormComponent } from './components/internships/newsletter-form/newsletter-form.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material';
 import { SharedModule } from '../shared/shared.module';
-import { MediacoverageComponent } from './components/mediacoverage/mediacoverage.component';
-import { CareersComponent } from './components/careers/careers.component';
-import { TncComponent } from './components/tnc/tnc.component';
-import { OurteamComponent } from './components/ourteam/ourteam.component';
-import { TeamMemberComponent } from './components/ourteam/team-member/team-member.component';
 
 @NgModule({
-  declarations: [InternshipsComponent, PerformersComponent, InternsComponent, MediacoverageComponent, CareersComponent, TncComponent, OurteamComponent, TeamMemberComponent],
+  declarations: [
+    InternshipsComponent,
+    PerformersComponent,
+    InternsComponent,
+    MediacoverageComponent,
+    CareersComponent,
+    TncComponent,
+    OurteamComponent,
+    TeamMemberComponent,
+    NewsletterFormComponent
+  ],
   imports: [
     CommonModule,
     BlogRoutingModule,
