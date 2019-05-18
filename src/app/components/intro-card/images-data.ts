ac5e4464f915c0f52fb6bee40ce04fd955076621
Ashish S(Manjaro) <s.cricket.ashish@gmail.com>
2019-05-18 16:22:42 +0530

    intro image webp

--- src/app/components/intro-card/images-data.ts
+++ src/app/components/intro-card/images-data.ts
@@ -1,33 +1,33 @@
 export const SLIDEIMAGES = [
   {
-    url: 'assets/images/slideshow/image1.jpg'
+    url: 'assets/images/slideshow/image1.webp'
   },
   {
-    url: 'assets/images/slideshow/image2.jpg'
+    url: 'assets/images/slideshow/image2.webp'
   },
   {
-    url: 'assets/images/slideshow/image3.jpg'
+    url: 'assets/images/slideshow/image3.webp'
   },
   {
-    url: 'assets/images/slideshow/image4.jpg'
+    url: 'assets/images/slideshow/image4.webp'
   },
   {
-    url: 'assets/images/slideshow/image5.jpg'
+    url: 'assets/images/slideshow/image5.webp'
   },
   {
-    url: 'assets/images/slideshow/image6.jpg'
+    url: 'assets/images/slideshow/image6.webp'
   },
   {
-    url: 'assets/images/slideshow/image7.jpg'
+    url: 'assets/images/slideshow/image7.webp'
   },
   {
-    url: 'assets/images/slideshow/image8.jpg'
+    url: 'assets/images/slideshow/image8.webp'
   },
   {
-    url: 'assets/images/slideshow/image9.jpg'
+    url: 'assets/images/slideshow/image9.webp'
   },
   {
-    url: 'assets/images/slideshow/image10.jpg'
+    url: 'assets/images/slideshow/image10.webp'
   }
 ];
 
@@ -42,75 +42,75 @@ export const SlideConfig = {
 
 export const PARTNERS = [
   {
-    url: 'assets/images/partners/iim-indore.jpeg'
+    url: 'assets/images/partners/iim-indore.webp'
   },
   {
-    url: 'assets/images/partners/atharv.png'
+    url: 'assets/images/partners/atharv.webp'
   },
   {
-    url: 'assets/images/partners/ranbhoomi18.png'
+    url: 'assets/images/partners/ranbhoomi18.webp'
   },
   {
-    url: 'assets/images/partners/deltech.jpeg'
+    url: 'assets/images/partners/deltech.webp'
   },
   {
-    url: 'assets/images/partners/tatva.jpg'
+    url: 'assets/images/partners/tatva.webp'
   },
   {
-    url: 'assets/images/partners/internshala.png'
+    url: 'assets/images/partners/internshala.webp'
   },
   {
-    url: 'assets/images/partners/shaheed-bhagat-logo.png'
+    url: 'assets/images/partners/shaheed-bhagat-logo.webp'
   }
 ];
 
 export const FEATUREDIMAGES = [
   {
-    url: 'assets/images/featured/du-admissions.png'
+    url: 'assets/images/featured/du-admissions.webp'
   },
   {
-    url: 'assets/images/featured/hindustantimes.jpg'
+    url: 'assets/images/featured/hindustantimes.webp'
   },
   {
-    url: 'assets/images/featured/freepress.jpg'
+    url: 'assets/images/featured/freepress.webp'
   },
   {
-    url: 'assets/images/featured/dsa.jpg'
+    url: 'assets/images/featured/dsa.webp'
   },
   {
-    url: 'assets/images/featured/insideiim.jpg'
+    url: 'assets/images/featured/insideiim.webp'
   },
   {
-    url: 'assets/images/featured/pathrika.jpg'
+    url: 'assets/images/featured/pathrika.webp'
   },
   {
-    url: 'assets/images/featured/techcircle.png'
+    url: 'assets/images/featured/techcircle.webp'
   },
   {
-    url: 'assets/images/featured/vccircle.png'
+    url: 'assets/images/featured/vccircle.webp'
   },
   {
-    url: 'assets/images/featured/amarujala.png'
+    url: 'assets/images/featured/amarujala.webp'
   }
 ];
 
 export const AWARDS = [
   {
-    url: 'assets/images/awards/utkarsha.png'
+    url: 'assets/images/awards/utkarsha.webp'
   },
   {
-    url: 'assets/images/awards/jaipuria.png'
+    url: 'assets/images/awards/jaipuria.webp'
   },
   {
-    url: 'assets/images/awards/eximius.png'
+    url: 'assets/images/awards/eximius.webp'
   },
   {
-    url: 'assets/images/awards/i5summit.png'
+    url: 'assets/images/awards/i5summit.webp'
   },
   {
-    url: 'assets/images/awards/iimiit.png'
+    url: 'assets/images/awards/iimiit.webp'
   },
   {
-    url: 'assets/images/awards/iexpo.png'
+    url: 'assets/images/awards/iexpo.webp'
   }
 ];
