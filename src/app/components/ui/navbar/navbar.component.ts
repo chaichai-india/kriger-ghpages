9784dc279d89d4f1a5574032e672bd7fd4cc4392
ashish_ <ashish@kriger.in>
2019-06-14 13:06:53 +0530

    navbar profile

--- src/app/components/ui/navbar/navbar.component.ts
+++ src/app/components/ui/navbar/navbar.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatSnackBar } from '@angular/material';
-import { DialogComponent } from '../../../post/components/post/post.component';
-import { AuthService } from 'src/app/services/authentication/auth.service';
-import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog, MatSnackBar } from "@angular/material";
+import { DialogComponent } from "../../../post/components/post/post.component";
+import { AuthService } from "src/app/services/authentication/auth.service";
+import { Router } from "@angular/router";
+import { BehaviorSubject } from "rxjs";
+import { takeWhile, take } from "rxjs/operators";
+import { UserService } from "src/app/services/database/user.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
   isNavbarCollapsed: boolean = true;
   isLoggedIn: boolean;
   isLoggedInObservable;
+  userDetails;
+
   constructor(
     private authService: AuthService,
+    private userService: UserService,
     public dialog: MatDialog,
     private router: Router,
     private _snackBar: MatSnackBar
@@ -28,19 +33,30 @@ export class NavbarComponent implements OnInit {
 
     this.authService.loggedInUpdateObservable().then(status => {
       this.isLoggedInObservable = status;
+      let copy = this.isLoggedInObservable;
+      copy.subscribe(res => (res ? this.getUser() : false));
     });
+  }
+
+  async getUser() {
+    const uid = await this.authService.userID;
+    const userdetails = await this.userService.getUserDetail(uid);
+    userdetails.pipe(take(1)).subscribe(details => {
+      this.userDetails = { uid, details };
+    });
+    console.log({ uid, userdetails });
   }
 
   async logout() {
     await this.authService.signout();
-    this.openSnackBar('Success!', 'Logged Out');
-    this.router.navigate(['/']);
+    this.openSnackBar("Success!", "Logged Out");
+    this.router.navigate(["/"]);
   }
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
       duration: 2000,
-      panelClass: 'success-dialog'
+      panelClass: "success-dialog"
     });
   }
 
--- src/app/services/authentication/auth.service.ts
+++ src/app/services/authentication/auth.service.ts
@@ -1,42 +1,42 @@
-import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { User } from 'firebase';
-import { first } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { User } from "firebase";
+import { first } from "rxjs/operators";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
   user: User;
   loggedInUpdate = new BehaviorSubject<boolean>(false);
 
   async login(email: string, password: string) {
-    let response = { message: 'Failed!!', action: 'OH NO' };
+    let response = { message: "Failed!!", action: "OH NO" };
     try {
       await this.afauth.auth
         .signInWithEmailAndPassword(email, password)
         .then(user => {
           // console.log('auth success');
-          response = { message: 'Success!', action: 'Logged In' };
+          response = { message: "Success!", action: "Logged In" };
           this.loggedInUpdate.next(true);
         })
         .catch(function(error) {
           // Handle Errors here.
           let errorCode = error.code;
           let errorMessage = error.message;
-          if (errorCode === 'auth/wrong-password') {
-            response = { message: 'Wrong Password!!', action: 'OH NO' };
-            alert('Wrong password.');
+          if (errorCode === "auth/wrong-password") {
+            response = { message: "Wrong Password!!", action: "OH NO" };
+            alert("Wrong password.");
           } else {
-            response = { message: errorMessage, action: 'Error' };
+            response = { message: errorMessage, action: "Error" };
             alert(errorMessage);
           }
           console.log(error);
         });
     } catch (e) {
-      response = { message: 'Failed!!', action: 'OH NO' };
-      throw new Error('auth failed');
+      response = { message: "Failed!!", action: "OH NO" };
+      throw new Error("auth failed");
     }
 
     return response;
@@ -68,6 +68,10 @@ export class AuthService {
 
   get userID() {
     return this.afauth.auth.currentUser.uid;
+  }
+
+  get userName() {
+    return this.afauth.auth.currentUser.displayName;
   }
 
   // getCurrentUser() {
