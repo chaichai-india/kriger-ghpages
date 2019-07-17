cea3cacd5370e07dd7efcceba3dd4fadd3a0045c
ashish_ <ashish@kriger.in>
2019-07-17 18:57:11 +0530

    new landing page

--- src/app/components/newlandingpage/newlandingpage.component.ts
+++ src/app/components/newlandingpage/newlandingpage.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-newlandingpage',
-  templateUrl: './newlandingpage.component.html',
-  styleUrls: ['./newlandingpage.component.css']
+  selector: "app-newlandingpage",
+  templateUrl: "./newlandingpage.component.html",
+  styleUrls: ["./newlandingpage.component.css"]
 })
 export class NewlandingpageComponent implements OnInit {
+  featured = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
