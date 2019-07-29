2e46eccd22fa8163d2f32bbe51e06a28f29cbb87
ashish_ <ashish@kriger.in>
2019-07-29 19:16:24 +0530

    font update, social media icon update, featured in

--- src/app/blog/components/ourteam/ourteam-data.ts
+++ src/app/blog/components/ourteam/ourteam-data.ts
@@ -1,161 +1,161 @@
 export const MENTORS = [
   {
-    firstname: 'Prof. D.L.',
-    lastname: 'Sunder',
-    job: 'Prof- IIM Indore, Ex. Dir.- NIFT Hyderabad',
-    linkedin: 'https://www.linkedin.com/in/sunderdl/',
+    firstname: "Prof. D.L.",
+    lastname: "Sunder",
+    job: "Prof- IIM Indore, Ex. Dir.- NIFT Hyderabad",
+    linkedin: "https://www.linkedin.com/in/sunderdl/",
     otherlink:
-      'https://www.iimidr.ac.in/faculty/full-time-faculty/profile/?id=81',
-    image: 'assets/images/ourteam/mentors/dl_sunder.jpg'
+      "https://www.iimidr.ac.in/faculty/full-time-faculty/profile/?id=81",
+    image: "assets/images/ourteam/mentors/dl_sunder.jpg"
   },
   {
-    firstname: 'Sandeep',
-    lastname: 'Rohilla',
-    job: 'MD, Orayan Group of Companies',
-    linkedin: 'https://www.linkedin.com/in/sandeep-rohilla-56069544/',
-    image: 'assets/images/ourteam/mentors/sandeep_rohilla.jpeg'
+    firstname: "Sandeep",
+    lastname: "Rohilla",
+    job: "MD, Orayan Group of Companies",
+    linkedin: "https://www.linkedin.com/in/sandeep-rohilla-56069544/",
+    image: "assets/images/ourteam/mentors/sandeep_rohilla.jpeg"
   }
 ];
 
 export const ADVISORS = [
   {
-    firstname: 'Sumeet',
-    lastname: 'Agrawal',
-    job: 'Financial Advisor',
-    linkedin: 'https://www.linkedin.com/in/sumeetagrawal/',
-    image: 'assets/images/ourteam/advisors/sumeet_agrawal.jpeg'
+    firstname: "Sumeet",
+    lastname: "Agrawal",
+    job: "Financial Advisor",
+    linkedin: "https://www.linkedin.com/in/sumeetagrawal/",
+    image: "assets/images/ourteam/advisors/sumeet_agrawal.jpeg"
   },
   {
-    firstname: 'Rajesh',
-    lastname: 'Jha',
-    job: 'Legal Advisor',
-    linkedin: 'https://www.linkedin.com/in/rajesh-jha-5b359322/',
-    image: 'assets/images/ourteam/advisors/rajesh_jha.jpeg'
+    firstname: "Rajesh",
+    lastname: "Jha",
+    job: "Legal Advisor",
+    linkedin: "https://www.linkedin.com/in/rajesh-jha-5b359322/",
+    image: "assets/images/ourteam/advisors/rajesh_jha.jpeg"
   }
 ];
 
 export const EXPERTS = [
   {
-    firstname: 'Ravi',
-    lastname: 'Kumar',
-    job: 'Founder, Modlingua Group',
+    firstname: "Ravi",
+    lastname: "Kumar",
+    job: "Founder, Modlingua Group",
     description:
-      'Ravi kumar is the founder of Hindi Center. He served as a faculty member for Spanish and Latin American Studies at Indian Institute of Foreign Trade (IIFT), New Delhi. He is a seasoned translator and interpreter & has worked as Simultaneous Interpreter for Prime Minister, Vice President and President of India.',
-    linkedin: 'https://www.linkedin.com/in/ravikumarin/',
+      "Ravi kumar is the founder of Hindi Center. He served as a faculty member for Spanish and Latin American Studies at Indian Institute of Foreign Trade (IIFT), New Delhi. He is a seasoned translator and interpreter & has worked as Simultaneous Interpreter for Prime Minister, Vice President and President of India.",
+    linkedin: "https://www.linkedin.com/in/ravikumarin/",
     otherlink:
-      'http://www.modlingua.com/director/281-ravi-kumar-modlingua.html',
-    image: 'assets/images/ourteam/experts/ravi_kumar.jpeg'
+      "http://www.modlingua.com/director/281-ravi-kumar-modlingua.html",
+    image: "assets/images/ourteam/experts/ravi_kumar.jpeg"
   },
   {
-    firstname: 'Dr. V. S. P',
-    lastname: 'Srivastav',
-    job: 'Joint Director, CD, IGNOU',
+    firstname: "Dr. V. S. P",
+    lastname: "Srivastav",
+    job: "Joint Director, CD, IGNOU",
     description:
-      'Dr. VSP Srivastav joined IGNOU as the Joint Director, Computer Division in 1996. He took over as director/head in 2000, continued till his retirement in 2016.  He is an alumnus of Indian Institute of Technology (IIT) Kanpur, and Jawaharlal Nehru University (JNU), New Delhi.',
-    linkedin: 'https://www.linkedin.com/in/dr-vijay-srivastav-a911131',
-    otherlink: 'https://drvspsrivastav.wordpress.com/computer-division/',
-    image: 'assets/images/ourteam/experts/vsp_srivastav.jpeg'
+      "Dr. VSP Srivastav joined IGNOU as the Joint Director, Computer Division in 1996. He took over as director/head in 2000, continued till his retirement in 2016.  He is an alumnus of Indian Institute of Technology (IIT) Kanpur, and Jawaharlal Nehru University (JNU), New Delhi.",
+    linkedin: "https://www.linkedin.com/in/dr-vijay-srivastav-a911131",
+    otherlink: "https://drvspsrivastav.wordpress.com/computer-division/",
+    image: "assets/images/ourteam/experts/vsp_srivastav.jpeg"
   },
   {
-    firstname: 'Sqn. Ldr. Manish',
-    lastname: 'Kumar (Retd.)',
-    job: 'Motivational Iife Coach',
+    firstname: "Sqn. Ldr. Manish",
+    lastname: "Kumar (Retd.)",
+    job: "Motivational Iife Coach",
     description:
-      'Manish Kumar is the Founder and President of Inspire Human Potential Development Limited (IHPDL). He is the mental training expert at Sport Authority of India (SAI). He is a best selling author and motivational speaker. He has been a Combat Pilot with the Indian Air Force where he was awarded the Chief of Air Staff Commendation.',
-    linkedin: 'https://www.linkedin.com/in/iinspire/',
-    otherlink: 'http://beyourownpilot.com/',
-    image: 'assets/images/ourteam/experts/manish_kumar.jpg'
+      "Manish Kumar is the Founder and President of Inspire Human Potential Development Limited (IHPDL). He is the mental training expert at Sport Authority of India (SAI). He is a best selling author and motivational speaker. He has been a Combat Pilot with the Indian Air Force where he was awarded the Chief of Air Staff Commendation.",
+    linkedin: "https://www.linkedin.com/in/iinspire/",
+    otherlink: "http://beyourownpilot.com/",
+    image: "assets/images/ourteam/experts/manish_kumar.jpg"
   },
   {
-    firstname: 'Prarthana',
-    lastname: 'Sharma',
-    job: 'Co-Director Aatma Prakash',
+    firstname: "Prarthana",
+    lastname: "Sharma",
+    job: "Co-Director Aatma Prakash",
     description:
-      'Miss Prarthana Sharma is the co-director of a mental health foundation (which is operating in Indore, Mumbai and Delhi). She is a visiting psychologist with TISS, Mumbai SVE-hub Indore. She actively works towards interventions with schools and neglected communities in Indore. In the past she has worked as a visiting psychologist with IIM, Indore.',
-    linkedin: 'https://www.linkedin.com/in/prarthanasharma34/',
-    otherlink: 'https://aatmaprakash.in/about/',
-    image: 'assets/images/ourteam/experts/prarthana_sharma.jpg'
+      "Miss Prarthana Sharma is the co-director of a mental health foundation (which is operating in Indore, Mumbai and Delhi). She is a visiting psychologist with TISS, Mumbai SVE-hub Indore. She actively works towards interventions with schools and neglected communities in Indore. In the past she has worked as a visiting psychologist with IIM, Indore.",
+    linkedin: "https://www.linkedin.com/in/prarthanasharma34/",
+    otherlink: "https://aatmaprakash.in/about/",
+    image: "assets/images/ourteam/experts/prarthana_sharma.jpg"
   },
   {
-    firstname: 'Dr. Shilpi',
-    lastname: 'Kukreja',
-    job: 'Psychologist & Career Counselor',
+    firstname: "Dr. Shilpi",
+    lastname: "Kukreja",
+    job: "Psychologist & Career Counselor",
     description:
-      'A doctorate in Psychology from the University of Delhi, Shilpi has clinical experience in working with people affected by psychological disorders at the Institute of Human Behaviour and Allied Sciences (IHBAS), Delhi. She has worked on projects involving domestic violence in the socially and economically backward areas, on ambiguity about career choices among college freshers in Delhi University.',
-    image: 'assets/images/ourteam/experts/shilpi_kukreja.jpeg'
+      "A doctorate in Psychology from the University of Delhi, Shilpi has clinical experience in working with people affected by psychological disorders at the Institute of Human Behaviour and Allied Sciences (IHBAS), Delhi. She has worked on projects involving domestic violence in the socially and economically backward areas, on ambiguity about career choices among college freshers in Delhi University.",
+    image: "assets/images/ourteam/experts/shilpi_kukreja.jpeg"
   },
   {
-    firstname: 'Rewati Raman',
-    lastname: 'Vishewar',
-    job: 'Co-Founder ‘The ZERO Curriculum’ & ‘Preschool For Child Rights’',
+    firstname: "Rewati Raman",
+    lastname: "Vishewar",
+    job: "Co-Founder ‘The ZERO Curriculum’ & ‘Preschool For Child Rights’",
     description:
-      'Raman has worked with several Education Companies, and has also Co-Founded ‘The ZERO Curriculum’ and ‘Preschool For Child Rights’. He has interest in Teacher Education, Curriculum, guiding in starting up new education ventures, Mentoring Education Entrepreneurs.',
-    linkedin: 'https://www.linkedin.com/in/iamstillstudyinginpreschool/',
-    image: 'assets/images/ourteam/experts/rewati_raman_vishewar.jpeg'
+      "Raman has worked with several Education Companies, and has also Co-Founded ‘The ZERO Curriculum’ and ‘Preschool For Child Rights’. He has interest in Teacher Education, Curriculum, guiding in starting up new education ventures, Mentoring Education Entrepreneurs.",
+    linkedin: "https://www.linkedin.com/in/iamstillstudyinginpreschool/",
+    image: "assets/images/ourteam/experts/rewati_raman_vishewar.jpeg"
   },
   {
-    firstname: 'Dr. Ajay',
-    lastname: 'Kumar',
-    job: 'Associate Professor, JNU',
+    firstname: "Dr. Ajay",
+    lastname: "Kumar",
+    job: "Associate Professor, JNU",
     description:
-      "Dr. Ajay Kumar is an Associate Professor at the Department of Adult Education, School of Social Sciences, JNU, New Delhi. He has been the Director of the Department of Adult Education. He is currently also the In-charge (Member-Secretary) of the university’s flagship ‘Community Engagement and Development Program’. Dr. Kumar has been the pioneer in introducing two new courses (first-of-its-kind) in an Indian university, namely ’Development Education' and 'Citizenship Education and Globalisation: Contemporary Issues and Perspectives'.He has worked with Government of India, various state agencies, ILO and UNESCO.",
-    otherlink: 'https://jnu.ac.in/content/ajaykumar',
-    image: 'assets/images/ourteam/experts/ajay_kumar.jpg'
+      "Dr. Ajay Kumar is an Associate Professor at the Department of Adult Education, School of Social Sciences, JNU. He is currently also the In-charge of the university’s flagship ‘Community Engagement and Development Program’. Dr. Kumar has been the pioneer in introducing two new courses. He has worked with Government of India, various state agencies, ILO and UNESCO.",
+    otherlink: "https://jnu.ac.in/content/ajaykumar",
+    image: "assets/images/ourteam/experts/ajay_kumar.jpg"
   },
   {
-    firstname: 'Rajesh',
-    lastname: 'Kumar',
-    job: 'PG-HRM BA-LLB.',
+    firstname: "Rajesh",
+    lastname: "Kumar",
+    job: "PG-HRM BA-LLB.",
     description:
       "A Mentor, Coach, and Guide for UPSC Examination with a brilliant track record. Earlier associated with Rau's IAS Study Circle in academics and Management(Bangalore and Delhi).He has more than 10 years of experience in the sphere of HR/ IR.Has earlier worked with Murugappa Group.He is also a certified auditor for ISO: 14001.",
-    image: 'assets/images/ourteam/experts/rajesh_kumar.jpeg'
+    image: "assets/images/ourteam/experts/rajesh_kumar.jpeg"
   }
 ];
 
 export const CORETEAM = [
   {
-    firstname: 'Neeraj',
-    lastname: 'Verma',
-    job: 'Co-founder & Operation',
-    linkedin: 'https://www.linkedin.com/in/neeraj-verma-380bb120/',
-    image: 'assets/images/ourteam/coreteam/neeraj_verma.jpeg'
+    firstname: "Neeraj",
+    lastname: "Verma",
+    job: "Co-founder & Operation",
+    linkedin: "https://www.linkedin.com/in/neeraj-verma-380bb120/",
+    image: "assets/images/ourteam/coreteam/neeraj_verma.jpeg"
   },
   {
-    firstname: 'Suraj',
-    lastname: 'Satyarthi',
-    job: 'Founder & Marketing',
-    linkedin: 'https://www.linkedin.com/in/surajsatyarthikriger/',
-    image: 'assets/images/ourteam/coreteam/suraj_satyarthi.png'
+    firstname: "Suraj",
+    lastname: "Satyarthi",
+    job: "Founder & Marketing",
+    linkedin: "https://www.linkedin.com/in/surajsatyarthikriger/",
+    image: "assets/images/ourteam/coreteam/suraj_satyarthi.png"
   },
   {
-    firstname: 'Chandan',
-    lastname: 'Satyarthi',
-    job: 'Co-founder & CTO',
-    linkedin: 'https://www.linkedin.com/in/chandansatyarthi/',
-    image: 'assets/images/ourteam/coreteam/chandan_satyarthi.jpg'
+    firstname: "Chandan",
+    lastname: "Satyarthi",
+    job: "Co-founder & CTO",
+    linkedin: "https://www.linkedin.com/in/chandansatyarthi/",
+    image: "assets/images/ourteam/coreteam/chandan_satyarthi.jpg"
   },
   {
-    firstname: 'Poojan',
-    lastname: 'Rathod',
-    job: 'Co-Founder & CAO',
-    linkedin: 'https://www.linkedin.com/in/poojan-rathod-8a2013137/',
-    image: 'assets/images/ourteam/coreteam/poojan_rathod.jpg'
+    firstname: "Poojan",
+    lastname: "Rathod",
+    job: "Co-Founder & CAO",
+    linkedin: "https://www.linkedin.com/in/poojan-rathod-8a2013137/",
+    image: "assets/images/ourteam/coreteam/poojan_rathod.jpg"
   }
 ];
 
 export const TECHNICALTEAM = [
   {
-    firstname: 'Neelu',
-    lastname: 'Maheshwari',
-    job: 'Software Developer',
-    linkedin: 'https://www.linkedin.com/in/neelu-maheshwari-63222aa8/',
-    image: 'assets/images/ourteam/technicalteam/neelu_maheshwari.jpeg'
+    firstname: "Neelu",
+    lastname: "Maheshwari",
+    job: "Software Developer",
+    linkedin: "https://www.linkedin.com/in/neelu-maheshwari-63222aa8/",
+    image: "assets/images/ourteam/technicalteam/neelu_maheshwari.jpeg"
   },
   {
-    firstname: 'Ashish',
-    lastname: 'Shaji',
-    job: 'Website Developer',
-    linkedin: 'https://www.linkedin.com/in/ashishshaji/',
-    image: 'assets/images/ourteam/technicalteam/ashish_shaji.jpeg'
+    firstname: "Ashish",
+    lastname: "Shaji",
+    job: "Website Developer",
+    linkedin: "https://www.linkedin.com/in/ashishshaji/",
+    image: "assets/images/ourteam/technicalteam/ashish_shaji.jpeg"
   }
 ];
