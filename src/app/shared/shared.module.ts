b8ed70397fe3cd7fb4d69371cf3e6a4068c39115
ashish_ <ashish@kriger.in>
2019-08-14 16:00:30 +0530

    footer new in blog

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from "@angular/material/button";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
-import { ContactComponent } from './contact/contact.component';
-import { ScrollTopComponent } from './scroll-top/scroll-top.component';
+import { ContactComponent } from "./contact/contact.component";
+import { ScrollTopComponent } from "./scroll-top/scroll-top.component";
+import { FooterNewComponent } from "./footer-new/footer-new.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [ContactComponent, ScrollTopComponent],
-  imports: [CommonModule, MatButtonModule, MatProgressSpinnerModule, NgbModule],
+  declarations: [ContactComponent, ScrollTopComponent, FooterNewComponent],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    NgbModule,
+    RouterModule
+  ],
   exports: [
     ContactComponent,
     ScrollTopComponent,
+    FooterNewComponent,
     CommonModule,
     MatProgressSpinnerModule,
     NgbModule
