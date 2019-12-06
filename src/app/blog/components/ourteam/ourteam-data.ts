61a06131429ec1065e42b1f13e54b02539067dd2
Ashish S <ashish@kriger.in>
2019-12-06 12:25:41 +0530

    Update: Our Team

--- src/app/blog/components/ourteam/ourteam-data.ts
+++ src/app/blog/components/ourteam/ourteam-data.ts
@@ -2,7 +2,7 @@ export const MENTORS = [
   {
     firstname: "Prof. D.L.",
     lastname: "Sunder",
-    job: "Prof- IIM Indore, Ex. Dir.- NIFT Hyderabad",
+    role: "Prof- IIM Indore, Ex. Dir.- NIFT Hyderabad",
     linkedin: "https://www.linkedin.com/in/sunderdl/",
     otherlink:
       "https://www.iimidr.ac.in/faculty/full-time-faculty/profile/?id=81",
@@ -11,7 +11,7 @@ export const MENTORS = [
   {
     firstname: "Sandeep",
     lastname: "Rohilla",
-    job: "MD, Orayan Group of Companies",
+    role: "MD, Orayan Group of Companies",
     linkedin: "https://www.linkedin.com/in/sandeep-rohilla-56069544/",
     image: "assets/images/ourteam/mentors/sandeep_rohilla.jpeg"
   }
@@ -21,14 +21,14 @@ export const ADVISORS = [
   {
     firstname: "Sumeet",
     lastname: "Agrawal",
-    job: "Financial Advisor",
+    role: "Financial Advisor",
     linkedin: "https://www.linkedin.com/in/sumeetagrawal/",
     image: "assets/images/ourteam/advisors/sumeet_agrawal.jpeg"
   },
   {
     firstname: "Rajesh",
     lastname: "Jha",
-    job: "Legal Advisor",
+    role: "Legal Advisor",
     linkedin: "https://www.linkedin.com/in/rajesh-jha-5b359322/",
     image: "assets/images/ourteam/advisors/rajesh_jha.jpeg"
   }
@@ -38,7 +38,7 @@ export const EXPERTS = [
   {
     firstname: "Ravi",
     lastname: "Kumar",
-    job: "Founder, Modlingua Group",
+    role: "Founder, Modlingua Group",
     description:
       "Ravi kumar is the founder of Hindi Center. He served as a faculty member for Spanish and Latin American Studies at Indian Institute of Foreign Trade (IIFT), New Delhi. He is a seasoned translator and interpreter & has worked as Simultaneous Interpreter for Prime Minister, Vice President and President of India.",
     linkedin: "https://www.linkedin.com/in/ravikumarin/",
@@ -49,7 +49,7 @@ export const EXPERTS = [
   {
     firstname: "Dr. V. S. P",
     lastname: "Srivastav",
-    job: "Joint Director, CD, IGNOU",
+    role: "Joint Director, CD, IGNOU",
     description:
       "Dr. VSP Srivastav joined IGNOU as the Joint Director, Computer Division in 1996. He took over as director/head in 2000, continued till his retirement in 2016.  He is an alumnus of Indian Institute of Technology (IIT) Kanpur, and Jawaharlal Nehru University (JNU), New Delhi.",
     linkedin: "https://www.linkedin.com/in/dr-vijay-srivastav-a911131",
@@ -59,7 +59,7 @@ export const EXPERTS = [
   {
     firstname: "Sqn. Ldr. Manish",
     lastname: "Kumar (Retd.)",
-    job: "Motivational Iife Coach",
+    role: "Motivational Iife Coach",
     description:
       "Manish Kumar is the Founder and President of Inspire Human Potential Development Limited (IHPDL). He is the mental training expert at Sport Authority of India (SAI). He is a best selling author and motivational speaker. He has been a Combat Pilot with the Indian Air Force where he was awarded the Chief of Air Staff Commendation.",
     linkedin: "https://www.linkedin.com/in/iinspire/",
@@ -69,7 +69,7 @@ export const EXPERTS = [
   {
     firstname: "Prarthana",
     lastname: "Sharma",
-    job: "Co-Director Aatma Prakash",
+    role: "Co-Director Aatma Prakash",
     description:
       "Miss Prarthana Sharma is the co-director of a mental health foundation (which is operating in Indore, Mumbai and Delhi). She is a visiting psychologist with TISS, Mumbai SVE-hub Indore. She actively works towards interventions with schools and neglected communities in Indore. In the past she has worked as a visiting psychologist with IIM, Indore.",
     linkedin: "https://www.linkedin.com/in/prarthanasharma34/",
@@ -79,7 +79,7 @@ export const EXPERTS = [
   {
     firstname: "Dr. Shilpi",
     lastname: "Kukreja",
-    job: "Psychologist & Career Counselor",
+    role: "Psychologist & Career Counselor",
     description:
       "A doctorate in Psychology from the University of Delhi, Shilpi has clinical experience in working with people affected by psychological disorders at the Institute of Human Behaviour and Allied Sciences (IHBAS), Delhi. She has worked on projects involving domestic violence in the socially and economically backward areas, on ambiguity about career choices among college freshers in Delhi University.",
     image: "assets/images/ourteam/experts/shilpi_kukreja.jpeg"
@@ -87,7 +87,7 @@ export const EXPERTS = [
   {
     firstname: "Rewati Raman",
     lastname: "Vishewar",
-    job: "Co-Founder ‘The ZERO Curriculum’ & ‘Preschool For Child Rights’",
+    role: "Co-Founder ‘The ZERO Curriculum’ & ‘Preschool For Child Rights’",
     description:
       "Raman has worked with several Education Companies, and has also Co-Founded ‘The ZERO Curriculum’ and ‘Preschool For Child Rights’. He has interest in Teacher Education, Curriculum, guiding in starting up new education ventures, Mentoring Education Entrepreneurs.",
     linkedin: "https://www.linkedin.com/in/iamstillstudyinginpreschool/",
@@ -96,7 +96,7 @@ export const EXPERTS = [
   {
     firstname: "Dr. Ajay",
     lastname: "Kumar",
-    job: "Associate Professor, JNU",
+    role: "Associate Professor, JNU",
     description:
       "Dr. Ajay Kumar is an Associate Professor at the Department of Adult Education, School of Social Sciences, JNU. He is currently also the In-charge of the university’s flagship ‘Community Engagement and Development Program’. Dr. Kumar has been the pioneer in introducing two new courses. He has worked with Government of India, various state agencies, ILO and UNESCO.",
     otherlink: "https://jnu.ac.in/content/ajaykumar",
@@ -105,7 +105,7 @@ export const EXPERTS = [
   {
     firstname: "Rajesh",
     lastname: "Kumar",
-    job: "PG-HRM BA-LLB.",
+    role: "PG-HRM BA-LLB.",
     description:
       "A Mentor, Coach, and Guide for UPSC Examination with a brilliant track record. Earlier associated with Rau's IAS Study Circle in academics and Management(Bangalore and Delhi).He has more than 10 years of experience in the sphere of HR/ IR.Has earlier worked with Murugappa Group.He is also a certified auditor for ISO: 14001.",
     image: "assets/images/ourteam/experts/rajesh_kumar.jpeg"
@@ -114,48 +114,55 @@ export const EXPERTS = [
 
 export const CORETEAM = [
   {
-    firstname: "Neeraj",
-    lastname: "Verma",
-    job: "Co-founder & Operation",
-    linkedin: "https://www.linkedin.com/in/neeraj-verma-380bb120/",
-    image: "assets/images/ourteam/coreteam/neeraj_verma.jpeg"
-  },
-  {
     firstname: "Suraj",
     lastname: "Satyarthi",
-    job: "Founder & Marketing",
+    role: "Founder & CEO",
     linkedin: "https://www.linkedin.com/in/surajsatyarthikriger/",
     image: "assets/images/ourteam/coreteam/suraj_satyarthi.png"
   },
   {
     firstname: "Chandan",
     lastname: "Satyarthi",
-    job: "Co-founder & CTO",
+    role: "Co-founder & CTO",
     linkedin: "https://www.linkedin.com/in/chandansatyarthi/",
     image: "assets/images/ourteam/coreteam/chandan_satyarthi.jpg"
   },
   {
     firstname: "Poojan",
     lastname: "Rathod",
-    job: "Co-Founder & CAO",
+    role: "Co-founder & CAO",
     linkedin: "https://www.linkedin.com/in/poojan-rathod-8a2013137/",
     image: "assets/images/ourteam/coreteam/poojan_rathod.jpg"
+  },
+  {
+    firstname: "Neeraj",
+    lastname: "Verma",
+    role: "Co-founder & Institute Partnership",
+    linkedin: "https://www.linkedin.com/in/neeraj-verma-380bb120/",
+    image: "assets/images/ourteam/coreteam/neeraj_verma.jpeg"
   }
 ];
 
 export const TECHNICALTEAM = [
-  {
-    firstname: "Neelu",
-    lastname: "Maheshwari",
-    job: "Software Developer",
-    linkedin: "https://www.linkedin.com/in/neelu-maheshwari-63222aa8/",
-    image: "assets/images/ourteam/technicalteam/neelu_maheshwari.jpeg"
-  },
+  // {
+  //   firstname: "Neelu",
+  //   lastname: "Maheshwari",
+  //   role: "Software Developer",
+  //   linkedin: "https://www.linkedin.com/in/neelu-maheshwari-63222aa8/",
+  //   image: "assets/images/ourteam/technicalteam/neelu_maheshwari.jpeg"
+  // },
   {
     firstname: "Ashish",
     lastname: "Shaji",
-    job: "Website Developer",
+    role: "Software Developer",
     linkedin: "https://www.linkedin.com/in/ashishshaji/",
     image: "assets/images/ourteam/technicalteam/ashish_shaji.jpeg"
+  },
+  {
+    firstname: "Prakhar",
+    lastname: "Mishra",
+    role: "Android Developer",
+    linkedin: "https://www.linkedin.com/in/prakhar-mishra-1a2739184/",
+    image: "assets/images/ourteam/technicalteam/prakhar_mishra.jpeg"
   }
 ];
