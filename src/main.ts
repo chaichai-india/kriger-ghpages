bba38abab4af2e98ac4d4b7e87f8bd025521f4e6
Ashish S <ashish@kriger.in>
2020-03-03 17:35:18 +0530

    host: log removed

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
-  // if (window) {
-  //   window.console.log = function() {};
-  // }
+  if (window) {
+    window.console.log = function() {};
+  }
 }
 
 // document.addEventListener("DOMContentLoaded", () => {
