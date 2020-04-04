5610e577c488b0ef6990d5222424b3a75e6dc97a
Ashish S <ashish@kriger.in>
2020-04-04 18:08:13 +0530

    Fix: ssr

--- src/app/marketplace/sidedrawer/sidedrawer.component.ts
+++ src/app/marketplace/sidedrawer/sidedrawer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { DialogComponent } from "src/app/shared/dialog/dialog.component";
+import { DialogComponent } from "../../shared/dialog/dialog.component";
 import { MatDialog } from "@angular/material/dialog";
 
 @Component({
