418acebf03f6e28bac369779e1003ea90ce3b92a
Ashish S <ashish@kriger.in>
2020-05-09 17:03:38 +0530

    Fix: is_like extended post

--- src/app/post/post-resolver.service.ts
+++ src/app/post/post-resolver.service.ts
@@ -5,13 +5,14 @@ import {
   Router,
   ActivatedRoute,
 } from "@angular/router";
-import { PostService } from "../core";
+import { PostService, ProfileService } from "../core";
 // import { catchError, switchMap, tap } from "rxjs/operators";
 
 @Injectable()
 export class PostResolver implements Resolve<any> {
   constructor(
     private postService: PostService,
+    private profileService: ProfileService,
     private router: Router,
     private route: ActivatedRoute,
     private zone: NgZone
@@ -26,10 +27,17 @@ export class PostResolver implements Resolve<any> {
     });
   }
 
-  pathResolver(post_id: string) {
+  async pathResolver(post_id: string) {
     console.log({ post_id });
+    let user_id = "null";
+    try {
+      const user$ = await this.profileService.getUser();
+      user_id = await user$.toPromise().then(({ _id }) => _id);
+    } catch (error) {
+      console.log(error);
+    }
     return this.postService
-      .getExtendedPost({ post_id, user_id: "null" })
+      .getExtendedPost({ post_id, user_id })
       .toPromise()
       .then((data) => {
         console.log("pathResolver", { data });
