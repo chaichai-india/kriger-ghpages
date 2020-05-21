82daaca7e43a418d818aa444a7be06e9c82f0295
Ashish S <ashish@kriger.in>
2020-05-21 14:54:59 +0530

    the hindi media coverage

--- src/app/components/newlandingpage/images-data.ts
+++ src/app/components/newlandingpage/images-data.ts
@@ -26,55 +26,75 @@ export const PARTNERS = [
 export const FEATUREDIMAGES = [
   {
     url: "assets/images/featured/whatsapp.png",
+    alt: "whatsapp logo",
+  },
+  {
+    url: "assets/images/featured/the-hindu.png",
+    alt: "the hindu logo",
   },
   {
     url: "assets/images/featured/yourstory.png",
+    alt: "yourstory logo",
   },
   {
     url: "assets/images/featured/news18.svg",
+    alt: "news18 logo",
   },
   {
     url: "assets/images/featured/devdiscourse.png",
+    alt: "devdiscourse logo",
   },
   {
     url: "assets/images/featured/earlypad.png",
+    alt: "earlypad logo",
   },
 
   {
     url: "assets/images/featured/nxtstartup.png",
+    alt: "nxtstartup logo",
   },
   {
     url: "assets/images/featured/iim.jpg",
+    alt: "iim logo",
   },
   {
     url: "assets/images/featured/vccircle.png",
+    alt: "vccircle logo",
   },
 
   {
     url: "assets/images/featured/amarujala.png",
+    alt: "amar ujala logo",
   },
   {
     url: "assets/images/featured/pathrika.jpg",
+    alt: "pathrika logo",
   },
 
   {
     url: "assets/images/featured/hindustantimes.png",
+    alt: "hindustan times logo",
   },
   {
     url: "assets/images/featured/du-admission.png",
+    alt: "du admission logo",
   },
 
   {
     url: "assets/images/featured/freepress.jpg",
+    alt: "freepress logo",
   },
   {
     url: "assets/images/featured/techcircle.png",
+    alt: "techcircle logo",
   },
   {
     url: "assets/images/featured/dsa.jpg",
+    alt: "dsa logo",
   },
   {
     url: "assets/images/featured/dainikbhaskar.png",
+    alt: "dainik bhaskar logo",
   },
 ];
 
