653014d6d60b3c0b0540ad0e3dbb30c119749bb5
Ashish S <ashish@kriger.in>
2020-10-08 09:28:40 +0530

    Update: Suraj.role = Marketing Manager

--- src/app/blog/components/ourteam/ourteam-data.ts
+++ src/app/blog/components/ourteam/ourteam-data.ts
@@ -6,15 +6,15 @@ export const MENTORS = [
     linkedin: "https://www.linkedin.com/in/sunderdl/",
     otherlink:
       "https://www.iimidr.ac.in/faculty/full-time-faculty/profile/?id=81",
-    image: "assets/images/ourteam/mentors/dl_sunder.jpg"
+    image: "assets/images/ourteam/mentors/dl_sunder.jpg",
   },
   {
     firstname: "Sandeep",
     lastname: "Rohilla",
     role: "MD, Orayan Group of Companies",
     linkedin: "https://www.linkedin.com/in/sandeep-rohilla-56069544/",
-    image: "assets/images/ourteam/mentors/sandeep_rohilla.jpeg"
-  }
+    image: "assets/images/ourteam/mentors/sandeep_rohilla.jpeg",
+  },
 ];
 
 export const ADVISORS = [
@@ -23,14 +23,14 @@ export const ADVISORS = [
     lastname: "Agrawal",
     role: "Financial Advisor",
     linkedin: "https://www.linkedin.com/in/sumeetagrawal/",
-    image: "assets/images/ourteam/advisors/sumeet_agrawal.jpg"
+    image: "assets/images/ourteam/advisors/sumeet_agrawal.jpg",
   },
   {
     firstname: "Rajesh",
     lastname: "Jha",
     role: "Company Secretary",
     linkedin: "https://www.linkedin.com/in/rajesh-jha-5b359322/",
-    image: "assets/images/ourteam/advisors/rajesh_jha.jpg"
+    image: "assets/images/ourteam/advisors/rajesh_jha.jpg",
   },
   {
     firstname: "Jagmohan",
@@ -38,8 +38,8 @@ export const ADVISORS = [
     role: "Technical Advisor",
     extra: "Facebook (U.K.)",
     linkedin: "https://www.linkedin.com/in/medoc/",
-    image: "assets/images/ourteam/advisors/jagmohan_singh.jpg"
-  }
+    image: "assets/images/ourteam/advisors/jagmohan_singh.jpg",
+  },
 ];
 
 export const LEGAL = [
@@ -48,8 +48,8 @@ export const LEGAL = [
     lastname: "Anik",
     role: "",
     linkedin: "https://www.linkedin.com/in/adv-anik-a3904112/",
-    image: "assets/images/ourteam/legal/adv_anik.jpg"
-  }
+    image: "assets/images/ourteam/legal/adv_anik.jpg",
+  },
 ];
 
 export const EXPERTS = [
@@ -62,7 +62,7 @@ export const EXPERTS = [
     linkedin: "https://www.linkedin.com/in/ravikumarin/",
     otherlink:
       "http://www.modlingua.com/director/281-ravi-kumar-modlingua.html",
-    image: "assets/images/ourteam/experts/ravi_kumar.jpeg"
+    image: "assets/images/ourteam/experts/ravi_kumar.jpeg",
   },
   {
     firstname: "Dr. V. S. P",
@@ -72,7 +72,7 @@ export const EXPERTS = [
       "Dr. VSP Srivastav joined IGNOU as the Joint Director, Computer Division in 1996. He took over as director/head in 2000, continued till his retirement in 2016.  He is an alumnus of Indian Institute of Technology (IIT) Kanpur, and Jawaharlal Nehru University (JNU), New Delhi.",
     linkedin: "https://www.linkedin.com/in/dr-vijay-srivastav-a911131",
     otherlink: "https://drvspsrivastav.wordpress.com/computer-division/",
-    image: "assets/images/ourteam/experts/vsp_srivastav.jpeg"
+    image: "assets/images/ourteam/experts/vsp_srivastav.jpeg",
   },
   {
     firstname: "Sqn. Ldr. Manish",
@@ -82,7 +82,7 @@ export const EXPERTS = [
       "Manish Kumar is the Founder and President of Inspire Human Potential Development Limited (IHPDL). He is the mental training expert at Sport Authority of India (SAI). He is a best selling author and motivational speaker. He has been a Combat Pilot with the Indian Air Force where he was awarded the Chief of Air Staff Commendation.",
     linkedin: "https://www.linkedin.com/in/iinspire/",
     otherlink: "http://beyourownpilot.com/",
-    image: "assets/images/ourteam/experts/manish_kumar.jpg"
+    image: "assets/images/ourteam/experts/manish_kumar.jpg",
   },
   {
     firstname: "Prarthana",
@@ -92,7 +92,7 @@ export const EXPERTS = [
       "Miss Prarthana Sharma is the co-director of a mental health foundation (which is operating in Indore, Mumbai and Delhi). She is a visiting psychologist with TISS, Mumbai SVE-hub Indore. She actively works towards interventions with schools and neglected communities in Indore. In the past she has worked as a visiting psychologist with IIM, Indore.",
     linkedin: "https://www.linkedin.com/in/prarthanasharma34/",
     otherlink: "https://aatmaprakash.in/about/",
-    image: "assets/images/ourteam/experts/prarthana_sharma.jpg"
+    image: "assets/images/ourteam/experts/prarthana_sharma.jpg",
   },
   {
     firstname: "Dr. Shilpi",
@@ -100,7 +100,7 @@ export const EXPERTS = [
     role: "Psychologist & Career Counselor",
     description:
       "A doctorate in Psychology from the University of Delhi, Shilpi has clinical experience in working with people affected by psychological disorders at the Institute of Human Behaviour and Allied Sciences (IHBAS), Delhi. She has worked on projects involving domestic violence in the socially and economically backward areas, on ambiguity about career choices among college freshers in Delhi University.",
-    image: "assets/images/ourteam/experts/shilpi_kukreja.jpeg"
+    image: "assets/images/ourteam/experts/shilpi_kukreja.jpeg",
   },
   {
     firstname: "Rewati Raman",
@@ -109,7 +109,7 @@ export const EXPERTS = [
     description:
       "Raman has worked with several Education Companies, and has also Co-Founded ‘The ZERO Curriculum’ and ‘Preschool For Child Rights’. He has interest in Teacher Education, Curriculum, guiding in starting up new education ventures, Mentoring Education Entrepreneurs.",
     linkedin: "https://www.linkedin.com/in/iamstillstudyinginpreschool/",
-    image: "assets/images/ourteam/experts/rewati_raman_vishewar.jpeg"
+    image: "assets/images/ourteam/experts/rewati_raman_vishewar.jpeg",
   },
   {
     firstname: "Dr. Ajay",
@@ -118,7 +118,7 @@ export const EXPERTS = [
     description:
       "Dr. Ajay Kumar is an Associate Professor at the Department of Adult Education, School of Social Sciences, JNU. He is currently also the In-charge of the university’s flagship ‘Community Engagement and Development Program’. Dr. Kumar has been the pioneer in introducing two new courses. He has worked with Government of India, various state agencies, ILO and UNESCO.",
     otherlink: "https://jnu.ac.in/content/ajaykumar",
-    image: "assets/images/ourteam/experts/ajay_kumar.jpg"
+    image: "assets/images/ourteam/experts/ajay_kumar.jpg",
   },
   {
     firstname: "Rajesh",
@@ -126,32 +126,32 @@ export const EXPERTS = [
     role: "PG-HRM BA-LLB.",
     description:
       "A Mentor, Coach, and Guide for UPSC Examination with a brilliant track record. Earlier associated with Rau's IAS Study Circle in academics and Management(Bangalore and Delhi).He has more than 10 years of experience in the sphere of HR/ IR.Has earlier worked with Murugappa Group.He is also a certified auditor for ISO: 14001.",
-    image: "assets/images/ourteam/experts/rajesh_kumar.jpeg"
-  }
+    image: "assets/images/ourteam/experts/rajesh_kumar.jpeg",
+  },
 ];
 
 export const CORETEAM = [
   {
     firstname: "Suraj",
     lastname: "Satyarthi",
-    role: "Founder & CEO",
+    role: "Marketing Manager",
     linkedin: "https://www.linkedin.com/in/surajsatyarthikriger/",
-    image: "assets/images/ourteam/coreteam/suraj_satyarthi.png"
+    image: "assets/images/ourteam/coreteam/suraj_satyarthi.png",
   },
   {
     firstname: "Chandan",
     lastname: "Satyarthi",
     role: "Co-founder & CTO",
     linkedin: "https://www.linkedin.com/in/chandansatyarthi/",
-    image: "assets/images/ourteam/coreteam/chandan_satyarthi.jpg"
+    image: "assets/images/ourteam/coreteam/chandan_satyarthi.jpg",
   },
   {
     firstname: "Poojan",
     lastname: "Rathod",
     role: "Co-founder & CAO",
     linkedin: "https://www.linkedin.com/in/poojan-rathod-8a2013137/",
-    image: "assets/images/ourteam/coreteam/poojan_rathod.jpg"
-  }
+    image: "assets/images/ourteam/coreteam/poojan_rathod.jpg",
+  },
 ];
 // export const INSTITUTEPARTNERSHIP = [
 //   {
@@ -175,13 +175,13 @@ export const TECHNICALTEAM = [
     lastname: "Shaji",
     role: "Software Developer",
     linkedin: "https://www.linkedin.com/in/ashishshaji/",
-    image: "assets/images/ourteam/technicalteam/ashish_shaji.jpeg"
+    image: "assets/images/ourteam/technicalteam/ashish_shaji.jpeg",
   },
   {
     firstname: "Prakhar",
     lastname: "Mishra",
     role: "Android Developer",
     linkedin: "https://www.linkedin.com/in/prakhar-mishra-1a2739184/",
-    image: "assets/images/ourteam/technicalteam/prakhar_mishra.jpeg"
-  }
+    image: "assets/images/ourteam/technicalteam/prakhar_mishra.jpeg",
+  },
 ];
